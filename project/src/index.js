0779f6aad635b79acd45b3ea47fca8578e48adb6
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-16 20:05:43 +0000

    Remove JSX From Starter Project

--- project/src/index.js
+++ project/src/index.js
@@ -4,20 +4,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function App() {
-  return (
-    <div className="counter">
-      <h1>React Counter</h1>
-      <p className="click_desc">
-        You clicked <span>0</span> times
-      </p>
-      <div className="button_container">
-        <button>Add 1</button>
-        <button>multiply by 5</button>
-        <button>reset</button>
-      </div>
-    </div>
-  );
+  return 'Hello! 👋';
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(App(), rootElement);
