f33d4f44192d5ba90157761e994dee7996b0a912
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-20 11:10:14 +0000

    Challenge!

--- challenge/src/index.js
+++ challenge/src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
+// Alison
 function App() {
   // 1- Create a slice of state called `direction` that tracks which direction button is clicked
   // 2- Set its initial value to '-'
+const[direction, setDirection]= React.useState("-");
   return (
     <div className="buttons-container">
       {/* 3- Render your new slice of state in a h1 element */}
+      <h1>{direction}</h1>
       {/* 4- Update the state with the direction (in words) when each button is clicked */}
-      <button>&#11013;</button>
-      <button>&#11014;</button>
-      <button>&#11015;</button>
-      <button>&#10145;</button>
+      <button onClick={() => setDirection("left")} >&#11013;</button>
+      <button onClick={() => setDirection("up")} >&#11014;</button>
+      <button onClick={() => setDirection("down")} >&#11015;</button>
+      <button onClick={() => setDirection("right")} >&#10145;</button>
     </div>
   );
 }
