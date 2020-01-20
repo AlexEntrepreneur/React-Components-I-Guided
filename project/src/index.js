f05e0cd14a2baa2d26debd9958b0ebb8b129de74
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-20 10:13:56 +0000

    State!

--- project/src/index.js
+++ project/src/index.js
@@ -4,9 +4,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function TestComponent() {
+  const [someState, setSomeState] = React.useState('Initial');
+
+  setTimeout(() => {
+    setSomeState('State has changed!');
+  }, 2000);
+
   return (
-    <div className="box">Hello React!</div>
+  <div className="box">{someState}</div>
     );
+}
+
+// Dimos
+function WeAreBack(){
+  return (
+    <div className="box">We Are Back!</div>
+  );
+}
+
+// Austin
+function Student() {
+  return (
+    <div className="container" style={{border: "1px solid white"}}>
+      <h1>Name: Austin</h1>
+      <h2>Age: 99</h2>
+      <h2>Favorite Language: JavaScript</h2>
+      <p>Introduction: Hi, my name is Austin and I like JavaScript</p>
+    </div>
+  );
 }
 
 
