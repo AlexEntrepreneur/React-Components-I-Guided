e64f652d8aae0d9a36acfbfd67ebf0291b5e8527
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-20 09:42:54 +0000

    Creating our first component!

--- project/src/index.js
+++ project/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
+function TestComponent() {
+  return (
+    <div className="box">Hello React!</div>
+    );
+}
 
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(App(), rootElement);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<TestComponent />, rootElement);
